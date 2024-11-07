00af49020757c31c96a264fd6366fdfda9c7aea0
whynotjava <tinycreeperx@gmail.com>
2024-11-06 22:53:51 -0500

    more shader stuff

--- include/headerFiles/shaderClass.h
+++ include/headerFiles/shaderClass.h
@@ -10,13 +10,22 @@
 
 std::string get_file_contents(const char* filename);
 
-class Shader{
+class Shader
+{
+public:
+	// Reference ID of the Shader Program
+	GLuint ID;
+	// Constructor that build the Shader Program from 2 different shaders
+	Shader(const char* vertexFile, const char* fragmentFile);
 
-    public:
-        GLuint ID;
-        Shader(const char* vertexFile, const char* fragmentFIle);
-       
-        void Activate();
-        void Delete();
+	// Activates the Shader Program
+	void Activate();
+	// Deletes the Shader Program
+	void Delete();
+private:
+	// Checks if the different Shaders have compiled properly
+	void compileErrors(unsigned int shader, const char* type);
 };
+
+
 #endif
