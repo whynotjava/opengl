83af3b9161edfb449fb233ad383f6a6999bd3e0f
whynotjava <tinycreeperx@gmail.com>
2024-11-09 19:51:51 -0500

    broken 3d

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -25,7 +25,7 @@ Texture::Texture(const char* image, GLenum texType, GLenum slot, GLenum format, 
 	
 	std::cout<<"gen and activated texture"<<std::endl;
 
-	glTexParameteri(texType, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(texType, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
 	glTexParameteri(texType, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -47,7 +47,7 @@ Texture::Texture(const char* image, GLenum texType, GLenum slot, GLenum format, 
 
 	std::cout<<"unboud and freed tex"<<std::endl;
 
-    glEnable(texType);
+    // glEnable(texType);
 
     std::cout<<"texture loaded succseully!"<<std::endl;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -13,21 +13,32 @@
 #include"headerFiles/Texture.h"
 #include <math.h>
 #include<stb/stb_image.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
+
+const unsigned int width = 800;
+const unsigned int height = 800;
 
 // Vertices coordinates
 GLfloat vertices[] =
 {  //              COORDINATES   /      COLORS     //  vericies
-	-0.5f, -0.5f, 0.0f,    1.0f, 0.0f, 0.0f,		0.0f, 0.0f,
-	-0.5f, 0.5f, 0.0f,     0.0f, 1.0f, 0.0f,  		0.0f, 1.0f,
-	0.5f, 0.5f, 0.0f,      0.0f, 0.0f, 1.0f,		1.0f, 1.0f,
-	0.5f, -0.5f, 0.0f, 	   1.0f, 0.0f, 0.0f,		1.0f, 0.0f
+	-0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	-0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.5f, 0.0f, -0.5f,     0.83f, 0.70f, 0.44f,	0.0f, 0.0f,
+	 0.5f, 0.0f,  0.5f,     0.83f, 0.70f, 0.44f,	5.0f, 0.0f,
+	 0.0f, 0.8f,  0.0f,     0.92f, 0.86f, 0.76f,	2.5f, 5.0f
 };
 
 // Indices for vertices order
 GLuint indices[] =
 {
-	0, 2, 1,
-	0, 3, 2
+	0, 1, 2,
+	0, 2, 3,
+	0, 1, 4,
+	1, 2, 4,
+	2, 3, 4,
+	3, 0, 4
 
 };
 
@@ -48,7 +59,7 @@ int main()
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(800, 800, "openGl tests and other shit I do", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(width, height, "openGl tests and other shit I do", NULL, NULL);
 	// Error check if the window fails to create
 	if (window == NULL)
 	{
@@ -63,7 +74,7 @@ int main()
 	gladLoadGL();
 	// Specify the viewport of OpenGL in the Window
 	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, width, height);
     // std::cout<<"debug"<<std::endl;
 	// Generates Shader object using shaders defualt.vert and default.frag
 	Shader shaderProgram("../res/shaders/default-vert.txt", "../res/shaders/default-frag.txt");
@@ -90,30 +101,49 @@ int main()
 
 	// textures!!!!!!
 	std::string texPath = "../res/tex/";
-	Texture popCat((texPath + "pop_cat.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
-	popCat.texUnit(shaderProgram, "tex0", 0);
+	Texture brick((texPath + "openGL1.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGB, GL_UNSIGNED_BYTE);
+	brick.texUnit(shaderProgram, "tex0", 0);
 
 	
 	
-    glEnable(GL_TEXTURE_2D);
+    // glEnable(GL_TEXTURE_2D);
 	std::cout<<"entered loop"<<std::endl;
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
 		// Specify the color of the background
-		glClearColor(0.0f, 0.5f, 0.0f, 1.0f);
+		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		// Clean the back buffer and assign the new color to it
 		glClear(GL_COLOR_BUFFER_BIT);
 		// Tell OpenGL which Shader Program we want to use
 		
 		shaderProgram.Activate();
-        glUniform1f(uniID, 0.0f);
 
-		popCat.Bind();
+		glm::mat4 model = glm::mat4(1.0f);
+		glm::mat4 view = glm::mat4(1.0f);
+		glm::mat4 proj = glm::mat4(1.0f);
+
+		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
+		proj = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
+
+		int modelLoc =  glGetUniformLocation(shaderProgram.ID, "model");
+		glUniformMatrix4fv(modelLoc, 1 ,GL_FALSE, glm::value_ptr(model));
+
+		int viewLoc =  glGetUniformLocation(shaderProgram.ID, "view");
+		glUniformMatrix4fv(viewLoc, 1 ,GL_FALSE, glm::value_ptr(view));
+
+		int projLoc =  glGetUniformLocation(shaderProgram.ID, "proj");
+		glUniformMatrix4fv(projLoc, 1 ,GL_FALSE, glm::value_ptr(model));
+
+
+
+        glUniform1f(uniID, 0.5);
+
+		brick.Bind();
 		// Bind the VAO so OpenGL knows to use it
 		VAO1.Bind();
 		// Draw primitives, number of indices, datatype of indices, index of indices
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
 		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
 		// Take care of all GLFW events
@@ -127,7 +157,7 @@ int main()
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
-	popCat.Delete();
+	brick.Delete();
 	shaderProgram.Delete();
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
